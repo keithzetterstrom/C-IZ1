c10d3bd8033a5ef72583f9d2f4f2ad6965937442
keithzetterstrom <mariyapmx@mail.ru>
2020-03-05 16:20:29 +0300

    Добавлено деление функций по файлам.

---
+++ matrix_work.h
@@ -0,0 +1,15 @@
+#ifndef IZ1_MATRIX_WORK_H
+#define IZ1_MATRIX_WORK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int arrey_of_count_elements_initialization(int ** matrix, int * arrey_of_count_elements, int strings, int columns);
+void result_vector_initialization(int ** matrix, int ** result_vector, int strings, int columns);
+int ** delete_nulls_from_matrix(int ** matrix, int strings, int columns);
+void create_matrix(int ** matrix, int strings, int columns);
+void print_matrix(int ** matrix, int strings, int columns);
+
+#endif //IZ1_MATRIX_WORK_H

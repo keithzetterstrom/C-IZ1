47aa6747c05b0cfc58402a594db554c090d5103b
keithzetterstrom <mariyapmx@mail.ru>
2020-03-07 01:35:27 +0300

    Настройка cMake

--- include/matrix_work.h
+++ include/matrix_work.h
@@ -6,9 +6,10 @@
 #include <time.h>
 
 
-int arrey_of_count_elements_initialization(int ** matrix, int * arrey_of_count_elements, int strings, int columns);
+int array_of_count_elements_initialization(int ** matrix, int * arrey_of_count_elements, int strings, int columns);
 void result_vector_initialization(int ** matrix, int ** result_vector, int strings, int columns);
 int ** delete_nulls_from_matrix(int ** matrix, int strings, int columns);
+void matrix_free(int ** matrix, int strings);
 void create_matrix(int ** matrix, int strings, int columns);
 void print_matrix(int ** matrix, int strings, int columns);
 
